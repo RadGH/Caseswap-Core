55c04566ae77c143f1312e8555404a237fd080e3
Radley Sustaire <radleygh@gmail.com>
2015-04-02 23:53:08 -0700

    Corrected plugin url/page

--- caseswap-core.php
+++ caseswap-core.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: CaseSwap Core
-Version: 1.0.3
+Version: 1.0.4
 Plugin URI: http://www.caseswap.com/
 Description: Integrates Contact Form 7 and Paid Memberships Pro into a subscription-based "submit-a-case" system where visitors can submit their case to paying members of the website.
 Author: Radley Sustaire
@@ -10,9 +10,9 @@ Author URI: mailto:radleygh@gmail.com
 Copyright 2015 CaseSwap.com
 */
 
-define( 'CSCore_VERSION', '1.0.3' );
-define( 'CSCore_URL', plugins_url() );
-define( 'CSCore_PATH', dirname(__FILE__) );
+define( 'CSCore_VERSION', '1.0.4' );
+define( 'CSCore_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+define( 'CSCore_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 
 require_once( CSCore_PATH . '/classes/caseswap.php' );
 
