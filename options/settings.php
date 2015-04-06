28434de14c0155c3c2265de98c561a111c369d85
Radley <radleygh@gmail.com>
2015-04-05 23:21:24 -0700

    Added comments

--- options/settings.php
+++ options/settings.php
@@ -2,6 +2,10 @@
 /**
  * Displays the main settings page for CaseSwap Core
  *
+ * Fields managed:
+ *  investigator-types
+ *  states
+ *
  */
 
 global $CSCore;
@@ -19,9 +23,9 @@ $options = $CSCore->Options->get_options();
       <p class="description"><small>Investigators who sign up may select one or more Investigation Type. When an email is sent, it is sent to any active member who has selected that Investigator Type.</small></p>
     </td>
     <td>
-                <textarea class="wide" name="cs_options[investigator-types]" id="cs_options_investigator-types" cols="80" rows="6"><?php
-                  echo esc_textarea( implode( "\n", $options['investigator-types'] ) );
-                  ?></textarea>
+      <textarea class="wide" name="cs_options[investigator-types]" id="cs_options_investigator-types" cols="80" rows="6"><?php
+        echo esc_textarea( implode( "\n", $options['investigator-types'] ) );
+        ?></textarea>
       <p class="description">One investigator type per line.</p>
     </td>
   </tr>
@@ -33,9 +37,9 @@ $options = $CSCore->Options->get_options();
       <p class="description"><small>These states will be available for investigators during sign up. Only states which have at least one investigator will appear for visitors submitting a new case.</small></p>
     </td>
     <td>
-                <textarea class="wide" name="cs_options[states]" id="cs_options_states" cols="80" rows="6"><?php
-                  echo esc_textarea( implode( "\n", $options['states'] ) );
-                  ?></textarea>
+      <textarea class="wide" name="cs_options[states]" id="cs_options_states" cols="80" rows="6"><?php
+        echo esc_textarea( implode( "\n", $options['states'] ) );
+        ?></textarea>
       <p class="description">One state per line.</p>
     </td>
   </tr>
