860ba22de80ac93b6c66ea6cabdc4ff1a8647757
Radley <radleygh@gmail.com>
2015-04-10 00:33:03 -0700

    Added filter to loop through all matching user IDs and only keep those with the selected membership level (configurable in dashboard)

--- classes/caseswap-membership-premium.php
+++ classes/caseswap-membership-premium.php
@@ -16,7 +16,34 @@ if ( !class_exists('CSCore_Members') ) {
 
     public function __construct() {
       // This is in the plugins_loaded event. You can use init hooks here.
+      add_filter( 'cscore_filter_matching_investigators', array( &$this, "filter_matching_investigators") );
     }
 
+    public function filter_matching_investigators( $user_ids ) {
+      if ( empty($user_ids) ) return $user_ids;
+
+      // Get required member level
+      global $CSCore;
+      $options = $CSCore->Options->get_options();
+      $membership_level = (int) $options['membership-subscription-level'];
+
+      // Is membership level required?
+      if ( $membership_level < 1 ) return $user_ids;
+
+      // Begin collecting array of users who are the correct member level.
+      $valid_user_ids = array();
+      $MembershipFactory = Membership_Plugin::factory();
+
+      foreach( $user_ids as $user_id ) {
+        $member = $MembershipFactory->get_member( $user_id );
+
+        // Does this user have the required membership level? (Ignoring subscription type)
+        if ( $member->on_level( $membership_level, null ) ) {
+          $valid_user_ids[] = $user_id;
+        }
+      }
+
+      return $valid_user_ids;
+    }
   }
 }
